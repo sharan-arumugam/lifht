3f6a1e91165ff4c31ecf55eb0b388b184efa0c73
Sharan Arumugam <sharumugam@apple.com>
2018-01-18 17:56:00 +0530

    change password ajax

--- src/main/java/com/lti/lifht/controller/EmployeeController.java
+++ src/main/java/com/lti/lifht/controller/EmployeeController.java
@@ -2,20 +2,30 @@ package com.lti.lifht.controller;
 
 import static com.lti.lifht.constant.PatternConstant.HAS_ANY_ROLE_ADMIN;
 import static com.lti.lifht.constant.PatternConstant.HAS_ANY_ROLE_EMPLOYEE;
+import static org.springframework.http.ResponseEntity.accepted;
+import static org.springframework.http.ResponseEntity.status;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lti.lifht.entity.Employee;
 import com.lti.lifht.model.EmployeeBean;
 import com.lti.lifht.model.EntryDateBean;
 import com.lti.lifht.model.EntryPairBean;
@@ -25,6 +35,7 @@ import com.lti.lifht.model.request.DateSinglePs;
 import com.lti.lifht.model.request.RangeMultiPs;
 import com.lti.lifht.model.request.RangeSinglePs;
 import com.lti.lifht.service.AdminService;
+import com.lti.lifht.service.MiscService;
 
 @RestController
 @RequestMapping("/api")
@@ -32,6 +43,9 @@ public class EmployeeController {
 
 	@Autowired
 	AdminService service;
+
+	@Autowired
+	private MiscService miscService;
 
 	@GetMapping("/all")
 	@PreAuthorize(HAS_ANY_ROLE_ADMIN)
@@ -66,6 +80,21 @@ public class EmployeeController {
 	@PreAuthorize(HAS_ANY_ROLE_ADMIN)
 	public List<EntryRange> getRangeMulti(@RequestBody RangeMultiPs request) {
 		return service.getRangeMulti(request, false);
+	}
+
+	@PostMapping("/changepassword")
+	@PreAuthorize(HAS_ANY_ROLE_EMPLOYEE)
+	public ResponseEntity<Object> changePassowrd(@RequestBody Map<String, String> requestParams, HttpSession session) {
+
+		Employee employee = miscService.changePassword(requestParams.get("currentPass"),
+				requestParams.get("newPass"),
+				session.getAttribute("psNumber").toString());
+
+		if (null != employee) {
+			return accepted().build();
+		} else {
+			return status(HttpStatus.PRECONDITION_FAILED).build();
+		}
 	}
 
 }
--- src/main/resources/static/libs/js/password.js
+++ src/main/resources/static/libs/js/password.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
-  /** password **/
+	/** password * */
 	$('#changePassword').on('submit', function(submitEvent) {
-        var currentPass = btoa(btoa($("#currentPassword").val()));
-        var newPass = btoa(btoa($("#newPassword").val()));
-        $.ajax({
-            method: "POST",
-            url: "change",
-            data: JSON.stringify({
-		            	currentPass: btoa(currentPass),
-		            	newPass: btoa(newPass)
-            		}),
-            contentType:"application/json; charset=utf-8",
-            dataType:"json",
-            success: function(response) {
-	            alert(response);
-            }
-          });
-        submitEvent.preventDefault();
-    });
-  /** password end **/
+		var currentPass = btoa(btoa($("#currentPassword").val()));
+		var newPass = btoa(btoa($("#newPassword").val()));
+		$.ajax({
+			method : "POST",
+			url : "api/changepassword",
+			data : JSON.stringify({
+				currentPass : btoa(currentPass),
+				newPass : btoa(newPass)
+			}),
+			contentType : "application/json; charset=utf-8",
+			dataType : "json",
+			success : function(response) {
+				alert(response);
+			}
+		});
+		submitEvent.preventDefault();
+	});
+	/** password end * */
 });
